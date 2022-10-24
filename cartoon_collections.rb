afaf1eaf7aede993f2243ba77d5bb77aed9156f7
fliblip <wickynjy@gmail.com>
2022-10-24 20:00:03 +0300

    Completed assignment

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,26 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves dwarves
   # Your code here
+ dwarves.map.with_index do
+  |dwarf, index| puts "#{index + 1}. #{dwarf}" 
+ end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet array
   # Your code here
+  array.map { |elem| elem.capitalize + "!" }
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls array
   # Your code here
+  array.any? {|call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese array
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |item|
+    found = cheese_types.find {|cheese| cheese == item}
+    !found.nil?
+  end
+  
 end
